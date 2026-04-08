4e59192f220942f25a098a145e25c605e3daaaf3
Wendy Moraes <wmoraes@zeus.vwu.edu>
2026-04-08 15:55:41 -0400

    Test: Commiting my first php file.

---
+++ area.php
@@ -0,0 +1,19 @@
+<!--
+	File name:
+	Assigment:
+	Due:
+	Author:
+	Goal:
+-->
+
+<center>
+<h2> Area Of Circle </h2>
+
+Assume the area of the circle is 1, then the area of the circle is:
+
+<p> Area = 3.14<hr></p>
+<p>
+If the radius is 1, the circumference of the circle is:
+</p>
+Circumference = 0;
+</center>
