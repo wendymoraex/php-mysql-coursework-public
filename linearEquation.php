8316d5624164f90c9f2e20cc463b87abcdaa1876
Wendy Moraes <137128829+wendymoraex@users.noreply.github.com>
2026-04-08 21:37:34 -0400

    Delete unnecessary comment lines

--- linearEquation.php
+++ linearEquation.php
@@ -3,27 +3,17 @@
 <center>
 <font color=blue>
 <?php
-	/* 
-		title comments
-	*/
-
-
 	$a = $_POST['aVar'];
 	$b = $_POST['bVar'];
 
-// debugging	
-//	print "a = $a, b = $b<P>";
-
-	if($a == 0)
-	{
+	if($a == 0) {
 		print "No real root.";
 	}
-	else
-	{
+	else {
+		
 		$x = -$b/$a;
-
 		$x = number_format($x, 2);
-
+		
 		print "Root: $x";
 	}
 ?>
