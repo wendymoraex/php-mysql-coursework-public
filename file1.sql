f5e166f56aaa30448acd965a9d9827a6b3568a26
Wendy Moraes <wendymoraes@Wendys-MacBook-Air-2.local>
2026-04-13 21:35:27 -0400

    Organized files into their respective folders

--- file1.sql
+++
@@ -1,5 +0,0 @@
-drop table STUDENT;
-create table STUDENT (ID int, FIRSTNAME varchar(20), LASTNAME varchar(20), GENDER varchar(6));
-insert into STUDENT values (1001, "Josh", "Smith", "Male");
-insert into STUDENT values (1002, "Wendy", "Moraes", "Female");
-select * values from STUDENT;
